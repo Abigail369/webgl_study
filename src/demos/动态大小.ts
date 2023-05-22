3c0d18a99cfbe8abbcea313c80c9b225e70e4c42
张圆 <17853312460@163.com>
2023-05-22 17:20:48 +0800

    add: 动态大小

--- src/demos/动态大小.ts
+++ src/demos/动态大小.ts
@@ -58,26 +58,57 @@ function drawPoint() {
   gl.linkProgram(program);
   gl.useProgram(program);
 
-  const vertices = new Float32Array([0.0, 0.0, 0.5, 0.5, 0.5, -0.5]);
-  var sizes = new Float32Array([10.0, 20.0, 30.0]);
+  // const vertices = new Float32Array([0.0, 0.0, 0.5, 0.5, 0.5, -0.5]);
+  // var sizes = new Float32Array([10.0, 20.0, 30.0]);
+  // const n = 3;
+
+  // let a_Position = gl.getAttribLocation(program, "a_Position");
+  // let a_PointSize = gl.getAttribLocation(program, "a_PointSize");
+
+  // const vertexBuffer = gl.createBuffer();
+  // gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  // gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+  // gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+  // gl.enableVertexAttribArray(a_Position);
+
+  // const sizeBuffer = gl.createBuffer();
+  // gl.bindBuffer(gl.ARRAY_BUFFER, sizeBuffer);
+  // gl.bufferData(gl.ARRAY_BUFFER, sizes, gl.STATIC_DRAW);
+  // gl.vertexAttribPointer(a_PointSize, 1, gl.FLOAT, false, 0, 0);
+  // gl.enableVertexAttribArray(a_PointSize);
+
+  // gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+  // gl.drawArrays(gl.POINTS, 0, n);
+
+  // 使用gl.vertexAttribPointer()的步进和偏移参数
+  const verticesSizes = new Float32Array([
+    0.0,
+    0.0,
+    10.0, //第1个点
+    0.5,
+    0.5,
+    20.0, //第2个点
+    0.5,
+    -0.5,
+    30.0, //第3个点
+  ]);
   const n = 3;
+  // 类型化数组具有 BYTES_PER_ELEMENT 属性，表示这种数据类型占据的字节数
+  // Float32Array.BYTES_PER_ELEMENT // 4
+  const FSIZE = verticesSizes.BYTES_PER_ELEMENT;
 
   let a_Position = gl.getAttribLocation(program, "a_Position");
   let a_PointSize = gl.getAttribLocation(program, "a_PointSize");
 
   const vertexBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+  gl.bufferData(gl.ARRAY_BUFFER, verticesSizes, gl.STATIC_DRAW);
+  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 3, 0);
   gl.enableVertexAttribArray(a_Position);
 
-  const sizeBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, sizeBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, sizes, gl.STATIC_DRAW);
-  gl.vertexAttribPointer(a_PointSize, 1, gl.FLOAT, false, 0, 0);
+  gl.vertexAttribPointer(a_PointSize, 1, gl.FLOAT, false, FSIZE * 3, FSIZE * 2);
   gl.enableVertexAttribArray(a_PointSize);
-
-  gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
   gl.drawArrays(gl.POINTS, 0, n);
 }
